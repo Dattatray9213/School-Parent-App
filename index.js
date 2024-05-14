e898788bd8850859c2bec4d08866315d81b88500
Shashank8624 <85009947+Shashank8624@users.noreply.github.com>
2024-05-14 14:20:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export * from "./About";
+export * from "./Examination";
+export * from "./Home";
+export * from "./Class";
+export * from "./Careers";
+export * from "./AdmissionProcess";
+
+export * from "./Register";
