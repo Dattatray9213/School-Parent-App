62985b1dd6c970f2e800471fcf627128d50e88f4
Dattatray9213 <161313205+Dattatray9213@users.noreply.github.com>
2024-05-15 12:29:56 +0530

    Add files via upload

---
+++ ParentsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using School_Parent_App.Data;
+using School_Parent_App.Models;
+
+namespace School_Parent_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParentsController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        public ParentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+                
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddParent(Parent parent)
+        {
+           
+            // Generate random value with in specified range 
+            long registrationId = GenerateRandomRegistrationId();
+            parent.RegistrationId = registrationId;
+
+            dbContext.Parents.Add(parent);
+            
+            await dbContext.SaveChangesAsync();
+            return Ok(parent);
+
+        }
+
+        private long GenerateRandomRegistrationId()
+        {
+            Random random = new Random();
+            long registrationId = ((long)random.Next(10000, 99999) * 100000) + (long)random.Next(10000, 99999);
+            return registrationId;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllParents()
+        {
+            var parents = await dbContext.Parents.ToListAsync();
+            return Ok(parents);
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> EditParent([FromRoute] int id, Parent parent)
+        {
+
+            var parents = await dbContext.Parents.FirstOrDefaultAsync(x => x.ParentId == parent.ParentId);
+            dbContext.Entry(parents).CurrentValues.SetValues(parent);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(parent);
+
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> DeleteParent([FromRoute] int id)
+        {
+            var parent = await dbContext.Parents.FirstOrDefaultAsync(x => x.ParentId == id);
+            dbContext.Parents.Remove(parent);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(parent);
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ StaffsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using School_Parent_App.Data;
+
+namespace School_Parent_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffsController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        public StaffsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllParents()
+        {
+            var parents = await dbContext.Parents.ToListAsync();
+            return Ok(parents);
+
+        }
+
+
+        [HttpPut("approve/{ParentId}")]
+        public async Task<IActionResult> ApproveParent(int ParentId)
+        {
+            var parent = await dbContext.Parents.FindAsync(ParentId);
+
+
+            // Update parent application status to Approved 
+            parent.Status = "Approved";
+            dbContext.Entry(parent).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+        [HttpPut("Reject/{ParentId}")]
+        public async Task<IActionResult> RejectParent(int ParentId)
+        {
+            var parent = await dbContext.Parents.FindAsync(ParentId);
+
+
+            // Update parent application status to Approved 
+            parent.Status = "Rejected";
+            dbContext.Entry(parent).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
