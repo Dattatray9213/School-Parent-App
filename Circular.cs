3627311e74f233ba80a616c76cc77458a122d9a9
Dattatray9213 <161313205+Dattatray9213@users.noreply.github.com>
2024-05-15 12:31:17 +0530

    Add files via upload

---
+++ Circular.cs
@@ -0,0 +1,12 @@
+﻿namespace School_Parent_App.Models
+{
+    public class Circular
+    {
+        public int Id { get; set; }
+        public DateTime NotificationDate { get; set; }
+        public string InformationText { get; set; }
+        public string NotificationPostedBy { get; set; }
+
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Win32;
+
+namespace School_Parent_App.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string  StudentRegisterNumber { get; set; }
+        public string StudentName { get; set; }
+
+    }
+}
