0010bc2c37867fda77eb0e3e28c9ca90c943f298
Shashank8624 <85009947+Shashank8624@users.noreply.github.com>
2024-05-22 12:15:58 +0530

    Update CreateNewCircular.jsx

--- CreateNewCircular.jsx
+++ CreateNewCircular.jsx
@@ -43,3 +43,48 @@ export default function CreateNewCircular() {
     </>
   );
 }
+
+
+
+import React from "react";
+import "./CreateNewCircular.css";
+
+export default function CreateNewCircular() {
+  return (
+    <>
+      <div className="circular-main-container">
+        <div className="create-new-circular-container">
+          <h1>Create new Circular</h1>
+        </div>
+        <div className="main-text-circular-container">
+          <div className="Circular-title">
+            <input
+              type="text"
+              name="circular-title"
+              placeholder="Circular heading"
+            />
+          </div>
+          <div className="Circular-text">
+            <input
+              type="text"
+              name="create-circular"
+              placeholder="Write your circular"
+            />
+          </div>
+          <br></br>
+          <div className="circular-bottom">
+            <div className="clr-poasted-by">
+              <input type="text" name="staff-name" placeholder="Staff Name" />
+            </div>
+            <div className="posted-date ">
+              <input type="Date" />
+            </div>
+          </div>
+        </div>
+        <div className="submit-circular">
+          <button type="submit">Submit</button>
+        </div>
+      </div>
+    </>
+  );
+}
